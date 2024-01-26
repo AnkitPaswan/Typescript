0cc87c7c1c7b049091488fa6c713d13e4d87d9bd
Ankit Paswan <ankitpaswan192@gmail.com>
2024-01-26 14:30:11 +0530

    ts-variables

--- intro.ts
+++ intro.ts
@@ -7,21 +7,21 @@
 
 
 
-let Fname: string = "Ankit";
+// let Fname: string = "Ankit";
 // let Fname = "Ankit";
 
 // Fname = 315
 
 // Fname = true 
 
-console.log(Fname);
-if (typeof Fname === 'string') {
-    console.log('Name is a String');
-} else if (typeof Fname === 'number') {
-    console.log('Name is a Number');
-} else if (typeof Fname === 'boolean') {
-    console.log("Its a boolean value");
-}
+// console.log(Fname);
+// if (typeof Fname === 'string') {
+//     console.log('Name is a String');
+// } else if (typeof Fname === 'number') {
+//     console.log('Name is a Number');
+// } else if (typeof Fname === 'boolean') {
+//     console.log("Its a boolean value");
+// }
 
 
 
@@ -30,6 +30,39 @@ if (typeof Fname === 'string') {
 var age: number = 32; // number variable
 var Name: string = "Ankit";// string variable
 var isUpdated: boolean = true;// Boolean variable
+
+
+// Example: Type Annotation of Parameters
+
+function display(id: number, name: string) {
+    console.log("Id = " + id + ", Name = " + name);
+}
+
+
+
+// Example: Type Annotation in Object 
+var employee: {
+    id: number;
+    name: string;
+};
+
+employee = {
+    id: 100,
+    name: "John"
+}
+
+
+// TypeScript - Variable
+
+var Fname = "Ankit";
+
+let Lname = "Paswan";
+
+const number = 315;
+
+console.log(Fname, Lname, number);
+
+
 
 
 
