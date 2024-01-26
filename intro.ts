9f6a19b783b886676175232f4f52019d2c77166a
Ankit Paswan <ankitpaswan192@gmail.com>
2024-01-26 14:28:41 +0530

    type-annotation

--- intro.ts
+++ intro.ts
@@ -1,8 +1,35 @@
 
 
 
-let Fname = "Ankit";
-var age = 25;
+// let Fname = "Ankit";
+// var age = 25;
+// console.log(Fname);
+
+
+
+let Fname: string = "Ankit";
+// let Fname = "Ankit";
+
+// Fname = 315
+
+// Fname = true 
+
 console.log(Fname);
+if (typeof Fname === 'string') {
+    console.log('Name is a String');
+} else if (typeof Fname === 'number') {
+    console.log('Name is a Number');
+} else if (typeof Fname === 'boolean') {
+    console.log("Its a boolean value");
+}
+
+
+
+// Example: Type Annotation in TypeScript 
+
+var age: number = 32; // number variable
+var Name: string = "Ankit";// string variable
+var isUpdated: boolean = true;// Boolean variable
+
 
 
